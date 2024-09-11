4eb1a2659ca27e27282d83ada0bee4aa3905ec2d
soudip-maity <soudipmaity605@gmail.com>
2024-09-11 21:35:18 +0530

    3rd

--- code.js
+++ code.js
@@ -3,12 +3,23 @@
 // console.log("hello3");
 let runagain=true;
 let email ="maity12@gmail.com"
+let password="maity"
 const login=(input_email)=>{
  return input_email==email?true:false
 }
 while(runagain){
     let input_email=prompt("enter email : ")
     if(input_email==email){
+        
+        break;
+    }
+}
+const login_password=(input_password)=>{
+ return input_password==email?true:false
+}
+while(runagain){
+    let input_password=prompt("enter password : ")
+    if(input_password==password){
         alert("login successfil")
         break;
     }
