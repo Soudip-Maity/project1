d0958ca326729715022473467025b462cbb832bc
soudip-maity <soudipmaity605@gmail.com>
2024-09-11 21:27:48 +0530

    second

--- code.js
+++ code.js
@@ -1,3 +1,15 @@
-console.log("hello");
-console.log("hello world");
-console.log("hello3");
+// console.log("hello");
+// console.log("hello world");
+// console.log("hello3");
+let runagain=true;
+let email ="maity12@gmail.com"
+const login=(input_email)=>{
+ return input_email==email?true:false
+}
+while(runagain){
+    let input_email=prompt("enter email : ")
+    if(input_email==email){
+        alert("login successfil")
+        break;
+    }
+}
