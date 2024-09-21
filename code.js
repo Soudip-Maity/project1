13eeef5de0552e2b91ef59571676e845303328c9
soudip-maity <soudipmaity605@gmail.com>
2024-09-21 13:17:22 +0530

    theme

--- code.js
+++ code.js
@@ -1,26 +1,77 @@
 // console.log("hello");
 // console.log("hello world");
 // console.log("hello3");
-let runagain=true;
-let email ="maity12@gmail.com"
-let password="maity"
-const login=(input_email)=>{
- return input_email==email?true:false
-}
-while(runagain){
-    let input_email=prompt("enter email : ")
-    if(input_email==email){
+// let runagain=true;
+// let email ="maity12@gmail.com"
+// let password="maity"
+// const login=(input_email)=>{
+//  return input_email==email?true:false
+// }
+// while(runagain){
+//     let input_email=prompt("enter email : ")
+//     if(input_email==email){
         
-        break;
-    }
+//         break;
+//     }
+// }
+// const login_password=(input_password)=>{
+//  return input_password==email?true:false
+// }
+// while(runagain){
+//     let input_password=prompt("enter password : ")
+//     if(input_password==password){
+//         alert("login successfil")
+//         break;
+//     }
+// }
+
+////////////////////////////////////////
+// reverse a String
+// let string = "soudip";
+// let arr =(''+string).split('')
+// // console.log(arr);
+// // for (let i=1;i<= arr.length;i++){
+// //    if(arr[i]===arr[i].at(arr.length)){
+// //     console.log(arr[i]);
+// //     arr.pop()
+    
+// //    }
+// // }
+// arr.reverse()
+// console.log(arr);
+
+///////////////////////////////////////////////////////////////////////
+// let btn1 = document.querySelector("#btn1");
+
+// btn1.onclick = (e) =>{
+//     alert("button was clicked")
+    
+// };
+
+
+
+
+// let div= document.querySelector("div");
+// div.onmouseover = (e) =>{   
+//    console.log(e.type);
+// };
+
+///////////////////////////////////////////////////////theme changing light to dark
+let modebtn = document.querySelector("#mode")
+let body = document.querySelector("body");
+let current_mode="light";
+
+
+modebtn.addEventListener("click", () => {
+if (current_mode==="light"){
+    current_mode="dark"
+    body.classList.add("dark")
+    body.classList.remove("light")
+}else{
+    current_mode="light"
+    body.classList.add("light")
+    body.classList.remove("dark")
 }
-const login_password=(input_password)=>{
- return input_password==email?true:false
-}
-while(runagain){
-    let input_password=prompt("enter password : ")
-    if(input_password==password){
-        alert("login successfil")
-        break;
-    }
-}
+console.log(current_mode);
+
+})
